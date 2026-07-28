64ab572ee9485099907b479b96478d60aaf3c059
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-07-28 22:41:09 +0600

    chore: initialize backend project setup

---
+++ rentNest-backend/src/server.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import { prisma } from "./lib/prisma";
+import app from "./app";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to Database Successfully");
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
